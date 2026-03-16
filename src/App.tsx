50d94bc1725b374abb184fd45d4cde132d52e33b
jeffyang0220 <jeff760220@gmail.com>
2026-03-16 17:40:43 +0800

    新版履歷上傳
    V1

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/Home';
+import Portfolio from './pages/Portfolio';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
+    </Router>
+  );
+}
